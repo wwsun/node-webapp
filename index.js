70c052970518b130b64da43aec8ceea86da543e7
Weiwei SUN <ww.sun@outlook.com>
2016-04-10 21:19:35 +0800

    feat: add a controller to handle the user requests

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 var server = require('./lib/server');
 var router = require('./lib/router');
+var controller = require('./lib/controller');
 
-server.startServer(4000, router.route);
+var handle = {};
+handle['/'] = controller.start;
+handle['/start'] = controller.start;
+handle['/upload'] = controller.upload;
+
+
+server.startServer(4000, router.route, handle);
